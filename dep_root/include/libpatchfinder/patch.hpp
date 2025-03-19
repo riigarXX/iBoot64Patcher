cf12cdf3f529bb2a22beba635b4fe7df65f9f2a4
Álvaro López <33065575+riigarXX@users.noreply.github.com>
2025-03-19 14:30:56 +0100

    .

--- dep_root/include/libpatchfinder/patch.hpp
+++ dep_root/include/libpatchfinder/patch.hpp
@@ -1,34 +1,17 @@
 // dep_root/include/libpatchfinder/patch.hpp
-
 #ifndef PATCH_HPP
 #define PATCH_HPP
 
-#include <cstddef>
-
 namespace tihmstar {
-namespace patchfinder {
-
-class patch {
-public:
-    // Constructor
-    patch(size_t patchSize, void* patchData)
-        : _patchSize(patchSize), _patch(patchData) {}
-
-    // Public accessor methods
-    size_t getPatchSize() const {
-        return _patchSize;
+    namespace patchfinder {
+        class patch {
+            size_t _patchSize;
+            const void *_patch;
+        public:
+            inline size_t getPatchSize() const { return _patchSize; }
+            inline const void *getPatch() const { return _patch; }
+        };
     }
-
-    void* getPatch() const {
-        return _patch;
-    }
-
-private:
-    size_t _patchSize;
-    void* _patch;
-};
-
-} // namespace patchfinder
-} // namespace tihmstar
+}
 
 #endif // PATCH_HPP
